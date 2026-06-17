b8521435116b720c474b4ee25019e622e5f60c6b
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-17 09:00:31 +0530

    fix bug

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,7 @@
-function Button ({text,type,classes="bg-blue-500 text-white p-2 rounded-lg cursor-pointer text-lg"}){
+function Button ({text,type,disabled=false,onClick,className=""}){
     return (
         <>
-        <button type={type} className={`${classes}`}>{text}</button>
+        <button type={type} className={`bg-blue-500 text-white p-2 rounded-lg cursor-pointer text-lg  ${className}`} disabled={disabled} onClick={onClick}>{text}</button>
         </>
     )
 }
