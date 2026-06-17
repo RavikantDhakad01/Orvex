7e01f1972dfb3738ab68828270fb095a81ece7f2
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-17 09:00:58 +0530

    improve ui

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,12 +1,12 @@
-function Input({ label, type, placeholder, name, id, value, onChange, frontIcon,backIcon,disabled}) {
+function Input({ label, type, placeholder, name, id, value, onChange, frontIcon,backIcon}) {
     return (
         <>
-            <div className="flex flex-col gap-1 text-lg">
+            <div className="flex flex-col gap-1 text-base">
                 <label htmlFor={id} className="font-bold">{label}</label>
 
                 <div className="flex gap-2 border border-gray-400 rounded-md p-2 relative">
-                    <div className="cursor-pointer">{frontIcon}</div>
-                    <input type={type} name={name} placeholder={placeholder} id={id} value={value} onChange={onChange} className="outline-none"/>
+                    <div>{frontIcon}</div>
+                    <input type={type} name={name} placeholder={placeholder} id={id} value={value} onChange={onChange} className="outline-none w-full"/>
                     <div className="cursor-pointer absolute right-3">{backIcon}</div>
                 </div>
             </div>
