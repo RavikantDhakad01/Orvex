cd0ba83b7167fe0eb372e4e9b6c78d02570ed717
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-23 09:26:32 +0530

    mongodb connection setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
-console.log("hello")
+import dotenv from "dotenv"
+dotenv.config()
+import connectDb from "./db/index.js"
+
+const serverStart = async () => {
+    try {
+        await connectDb()
+
+    } catch (error) {
+        console.log("Mongodb Connection failure:", error)
+        process.exit(1)
+    }
+}
+
+serverStart()
