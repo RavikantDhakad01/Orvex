06476fa86b55cbdc629f94f88911ea6b41f098be
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-24 16:11:57 +0530

    fix typo

--- server/src/controllers/healthcheck.controllers.js
+++ server/src/controllers/healthcheck.controllers.js
@@ -1,9 +1,8 @@
-import ApiResponse from "../utils/ApiResponse";
+import ApiResponse from "../utils/ApiResponse.js";
 
 const healthCheck = async (req, res) => {
 
         return res.status(200).json(new ApiResponse(200, {}, "Server is running"))
-
 }
 
 export { healthCheck }
