f08351c9752954c8dcd5e3e6faca03c73e722bcd
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-24 15:17:22 +0530

    Add auth middleware

---
+++ server/src/middlewares/auth.middlewares.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken"
+import ApiError from "../utils/ApiError"
+import User from "../models/user.model"
+
+const verifyJwt = async (req, res, next) => {
+    try {
+        const { accessToken } = req.cookies || req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!accessToken) {
+            throw new ApiError(401, "Unauthorized access")
+        }
+
+        const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decoded._id).select("-password -refreshToken")
+
+        if (!user) {
+            throw new ApiError(401, "Invaid access token")
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {
+    verifyJwt
+}
