8e2d0f244605a70a2a1fd99c4234449765cab246
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-28 12:57:13 +0530

    Add dashboard page

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -4,14 +4,16 @@ import Signup from "../pages/Signup.jsx"
 import ForgotPassword from "../pages/ForgotPassword.jsx"
 import ResetPassword from "../pages/ResetPassword.jsx"
 import LandingPage from "../pages/LandingPage.jsx"
+import Dashboard from "../pages/Dashboard.jsx"
 
 function AppRoutes() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/reset-password" element={<ResetPassword />} />
             </Routes>
