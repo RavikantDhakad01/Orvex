0c25fe74bb29aa140e0957c0a71c380c869223bf
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-28 18:25:04 +0530

    current user api integration

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { LogOut, User, ChevronDown, Mail, } from "lucide-react";
 import { useState } from "react"
 
-function Navbar() {
+function Navbar({ user }) {
     const [open, setOpen] = useState(false)
     return (
         <header className="z-10 sticky top-0">
@@ -9,18 +9,18 @@ function Navbar() {
                 <ul className="bg-white shadow flex items-center justify-end p-4 border-b border-gray-200">
                     <li className="relative">
                         <button className="cursor-pointer flex items-center" onClick={() => setOpen(!open)}>
-                            <div className="text-blue-800 flex justify-center items-center w-7 h-7 bg-blue-200 rounded-full">{"R"}</div>
+                            <div className="text-blue-800 flex justify-center items-center w-7 h-7 bg-blue-200 rounded-full">{user?.username?.charAt(0).toUpperCase()}</div>
                             <ChevronDown size={16} />
                         </button>
                         {
                             open && (
                                 <div className="flex flex-col gap-2 absolute  right-0 top-11  w-64 bg-white shadow-md rounded-xl border border-gray-200 p-4 z-30">
                                     <div className="flex gap-2 items-center">
-                                        <User size={18} /><span>{"Ravikant"}</span></div>
+                                        <User size={18} /><span>{user?.username}</span></div>
                                     <div className="flex gap-2 items-center text-gray-600
 text-sm">
                                         <Mail size={18} />
-                                        <span>{"ravi@gmail.com"}</span>
+                                        <span>{user?.email}</span>
                                     </div>
 
                                     <button className="flex mt-3 pt-2 px-1 border-t border-gray-300 gap-2 items-center cursor-pointer hover:text-red-500
--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,14 +1,28 @@
 import Sidebar from "../components/Sidebar.jsx"
 import Navbar from "../components/Navbar.jsx"
 import { Outlet } from "react-router-dom"
+import { currentUser } from "../services/auth.services.js"
+import { useEffect, useState } from "react"
 
 function DashboardLayout() {
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        const getCurrentUser = async () => {
+            try {
+                const user = await currentUser()
+                setUser(user.data)
+            } catch (error) {
+                console.log(error.response.data.message)
+            }
+        }
+        getCurrentUser()
+    }, [])
     return (
         <div className="flex  w-full">
-            
+
             <Sidebar />
             <div className="w-full">
-                <Navbar />
+                <Navbar user={user} />
                 <main className="p-8">
                     <Outlet />
                 </main>
