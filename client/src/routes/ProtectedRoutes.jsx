bd747ffc047e375efc88687d37e12b5fb2af70fb
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-29 16:07:01 +0530

    Add loader component

--- client/src/routes/ProtectedRoutes.jsx
+++ client/src/routes/ProtectedRoutes.jsx
@@ -1,15 +1,12 @@
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
+import Loader from "../components/Loader";
 
 function ProtectedRoutes() {
     const { user, loading } = useContext(AuthContext)
     if (loading) {
-        return (
-            <div className="min-h-screen flex justify-center items-center">
-                 <h1>Loading...</h1>
-            </div>
-       )
+        return <Loader/>
     }
     if (!user) {
         return <Navigate to="/login" replace />;
