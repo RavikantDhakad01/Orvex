35d9930d2cb290526cdf5a2eb070f7b4db4a3816
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-06-30 17:49:28 +0530

    Add dynamic username

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
+import useAuth from "../hooks/useAuth.jsx"
 function Dashboard() {
-  
+const {user} = useAuth()
     return (
         <>
             <div className="flex flex-col gap-2">
                 <h1 className="font-bold text-2xl">Welcome to Orvex 🚀</h1>
-                <h3 className="font-semibold text-lg">Hello,{" Ravikant"}👋</h3>
+                <h3 className="font-semibold text-lg">Hello, { user?.username}👋</h3>
             </div>
         </>
     )
