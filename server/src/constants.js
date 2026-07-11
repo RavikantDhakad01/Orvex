ca7e0ee11ce045fdd1e2d64fdb84b508cc117a8b
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-11 17:42:41 +0530

    Add constants

---
+++ server/src/constants.js
@@ -0,0 +1,12 @@
+const WORKSPACE_ROLE = {
+    OWNER: "owner",
+    MEMBER: "member"
+}
+
+const INVITATION_STATUS = {
+    PENDING: "pending",
+    ACCEPTED: "accepted",
+    REJECTED: "rejected"
+}
+
+export { WORKSPACE_ROLE, INVITATION_STATUS }
