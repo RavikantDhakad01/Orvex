bfe8f38e86c16e2b2e47e7624bd5e23aaf643947
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-13 08:38:54 +0530

    Add controllers boiler plate code

---
+++ server/src/controllers/invitation.controllers.js
@@ -0,0 +1,42 @@
+import ApiError from "../utils/ApiError.js"
+import ApiResponse from "../utils/ApiResponse.js"
+import Invitation from "../models/invitation.model.js"
+
+const inviteMember= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getUserInvitations= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const acceptInvitation= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const rejectInvitation= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {
+    inviteMember,
+    getUserInvitations,
+    acceptInvitation,
+    rejectInvitation
+}
--- server/src/controllers/workspace.controllers.js
+++ server/src/controllers/workspace.controllers.js
@@ -1,3 +1,51 @@
 import ApiResponse from "../utils/ApiResponse.js"
 import ApiError from "../utils/ApiError.js"
 import Workspace from "../models/workspace.model.js"
+
+const createWorkspace= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getUserWorkspaces= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getWorkspaceById= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const updateWorkspace= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+const deleteWorkspace= async(req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {
+    createWorkspace,
+    getUserWorkspaces,
+    getWorkspaceById,
+    updateWorkspace,
+    deleteWorkspace
+}
