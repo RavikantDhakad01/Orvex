ba930f23621541da83601bd3de038389e6231431
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-21 10:30:43 +0530

    improve ui

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -1,6 +1,6 @@
 function Avatar(){
     return (
-      <div className="flex justify-center items-center bg-yellow-100 text-yellow-400 rounded-md h-12 w-12">
+      <div className="flex justify-center items-center bg-yellow-100 text-yellow-400 rounded-md h-12 p-3">
 OT
       </div>
     )
