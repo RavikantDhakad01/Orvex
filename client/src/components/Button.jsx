acb02b361703defeca2c9cd0c686efe81649171a
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-21 10:30:04 +0530

    Add tailwind merge

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,12 @@
+import { twMerge } from "tailwind-merge";
 function Button ({text,type,disabled=false,onClick,className=""}){
     return (
         <>
-        <button type={type} className={`bg-blue-500 text-white p-2 rounded-lg text-lg  ${className}`} disabled={disabled} onClick={onClick}>{text}</button>
+        <button type={type} className={twMerge(
+                "bg-blue-500 text-white p-2 rounded-lg text-lg",
+                className
+            )}
+            disabled={disabled} onClick={onClick}>{text}</button>
         </>
     )
 }
