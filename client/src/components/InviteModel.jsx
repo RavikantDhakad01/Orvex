c5cc27d557c3525c0139cea3af9ddbbf90b38bad
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-23 19:34:01 +0530

    Add invite member model

---
+++ client/src/components/InviteModel.jsx
@@ -0,0 +1,30 @@
+import { X } from "lucide-react"
+import Input from "../components/Input.jsx"
+import { useState } from "react"
+import TextArea from "../components/TestArea.jsx"
+import Button from "./Button.jsx"
+
+function InviteModel({ setModelOpen }) {
+    const [email, setEmail] = useState("")
+   
+    return (
+        <>
+            <div className="flex relative flex-col">
+                <h1 className="text-center font-bold text-xl">invite Member</h1>
+                <X size={26} strokeWidth={2} className="cursor-pointer absolute top-0 right-0" onClick={() => setModelOpen(false)} />
+                <form className="flex flex-col gap-8">
+                    <Input label="Email" type="text" placeholder="Enter email address" name="email" id="email" value={email} onChange={(e) => {
+                        setName(e.target.value)
+                    }} />
+
+                    <div className="flex justify-center gap-4">
+                        <Button text="Cancel" className="text-black border border-gray-600 bg-white "/>
+                         <Button text="Send Invitation"/>
+                    </div>
+                </form>
+            </div>
+
+        </>
+    )
+}
+export default InviteModel
