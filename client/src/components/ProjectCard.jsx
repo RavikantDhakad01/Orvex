024644068a26331c704f754ee08c8767b51730d1
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-23 13:44:49 +0530

    Add projects tab

---
+++ client/src/components/ProjectCard.jsx
@@ -0,0 +1,19 @@
+import Avatar from "./Avatar.jsx"
+import { EllipsisVertical } from "lucide-react"
+
+function ProjectCard() {
+  return (
+    <div className="flex justify-between items-center shadow-md py-6 px-4 rounded-lg">
+      <div className="flex gap-6 items-center">
+        <Avatar />
+        <div className="flex flex-col">
+          <h1 className="font-bold text-lg">Orvex Mobile</h1>
+          <p className="text-gray-600">Updated 2 days ago</p>
+        </div>
+      </div>
+      <EllipsisVertical />
+    </div>
+  )
+}
+export default ProjectCard
+
---
+++ client/src/components/projects.jsx
@@ -0,0 +1,18 @@
+import ProjectCard from "../components/ProjectCard.jsx"
+import Button from "./Button.jsx"
+
+function Projects({ className }) {
+    return (
+        <div className=" flex flex-col gap-10 mt-6">
+            <div  className=" flex flex-col gap-4">
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+            </div>
+            <Button text="+ Add Project" />
+        </div>
+    )
+}
+export default Projects
+
