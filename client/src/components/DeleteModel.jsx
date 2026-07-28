cf1c470b4b70fe948842f925c5e410166a49efe0
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-28 15:41:39 +0530

    improve ui

--- client/src/components/DeleteModel.jsx
+++ client/src/components/DeleteModel.jsx
@@ -1,27 +1,27 @@
 import { useState } from "react"
 import Button from "./Button.jsx"
-import {Trash2} from "lucide-react"
+import { Trash2 } from "lucide-react"
 
 function DeleteModel({ setIsModelOpen }) {
-    const [name, setName] = useState("Over Team")
-   const [description, setDescription] = useState("sjdj nasjf jansjnj bajj")
+
     return (
         <>
-          <div className="flex flex-col items-center gap-20 mt-14">
-            <div className="flex flex-col items-center gap-4">
-                <div className="flex items-center justify-center bg-red-100 w-22 h-22 rounded-full text-red-600">{<Trash2/>}</div>
-                <div className=" flex flex-col items-center gap-1">
-                    <h1 className="font-bold text-xl">{"Delete Workspace?"}</h1>
-                    <p className="text-lg text-gray-800">{"This action cannot be undone."}</p>
+            <div className="flex flex-col items-center gap-20 mt-14">
+                <div className="flex flex-col items-center gap-4">
+                    <div className="flex items-center justify-center bg-red-100 w-22 h-22 rounded-full text-red-600">{<Trash2 size={34}
+                        strokeWidth={1.75} />}</div>
+                    <div className=" flex flex-col items-center gap-1">
+                        <h1 className="font-bold text-xl">{"Delete Workspace?"}</h1>
+                        <p className="text-lg text-gray-800">{"This action cannot be undone."}</p>
+                    </div>
+
                 </div>
 
+                <div className="flex flex-col gap-4 w-full">
+                    <Button text="Delete Workspace" className="bg-red-600 py-3 " type="button" />
+                    <Button text="Cancel" className="text-black border border-gray-600 bg-white py-3" />
+                </div>
             </div>
-
-           <div className="flex flex-col gap-4 w-full">
-            <Button text="Delete Workspace" className="bg-red-600 py-3 "/>
-             <Button text="Cancel" className="text-black border border-gray-600 bg-white py-3"/>
-           </div>
-        </div>
 
         </>
     )
