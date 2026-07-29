25a45ebafebd1849171fca32d3ea84f87ae23b7a
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-29 18:30:18 +0530

    Add invitation services

---
+++ client/src/services/invitation.services.js
@@ -0,0 +1,29 @@
+import api from "../api/axios.js";
+
+const inviteMember = async (data) => {
+    const res = await api.post("/invitations/workspace/:workspaceId", data)
+    return res.data
+}
+
+const getUserInvitations = async () => {
+    const res = await api.get("/invitations")
+    return res.data
+}
+
+const acceptInvitation = async (data) => {
+    const res = await api.patch("/invitations/:invitationId", data)
+    return res.data
+}
+
+const rejectInvitation = async () => {
+    const res = await api.delete("/invitations/:invitationId")
+    return res.data
+}
+
+
+export {
+    inviteMember,
+    getUserInvitations,
+    acceptInvitation,
+    rejectInvitation
+}
