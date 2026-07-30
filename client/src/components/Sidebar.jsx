4af1a625837957136223b81546905a7fedf9a134
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-07-30 11:29:48 +0530

    Add active state

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom";
 import { LayoutDashboard, BriefcaseBusiness, FolderKanban, Mail, ListTodo,X} from "lucide-react";
 import logo from "../assets/logo.svg"
+import { NavLink } from "react-router-dom";
+
 function Sidebar({ isSidebarOpen, setIsSidebarOpen }) {
-    
+
     return (
         <aside className={`flex flex-col gap-7 w-64 bg-white h-screen p-3 shadow-lg border-r border-gray-200 z-40 fixed left-0 top-0  lg:static transition-transform duration-300 ease-in-out  ${isSidebarOpen?"translate-x-0":"-translate-x-full lg:translate-x-0"}`}>
             <div className="flex flex-row gap-2 relative">
@@ -14,33 +15,34 @@ function Sidebar({ isSidebarOpen, setIsSidebarOpen }) {
             <nav>
                 <ul className="flex flex-col gap-2">
                     <li>
-                        <Link to="/dashboard" className="flex gap-2  bg-blue-100 rounded-lg py-1 px-2 text-blue-700" onClick={()=> setIsSidebarOpen(false)}>
+                        <NavLink to="/dashboard" className={({isActive})=>`flex gap-2 rounded-lg py-1 px-2 transition-colors ${isActive?"bg-blue-100 text-blue-600":"text-slate-600 hover:bg-slate-100"}`} onClick={()=> setIsSidebarOpen(false)}>
+                        
                             <LayoutDashboard size={22} className="font-bold" /> <span className="text-base ">Dashboard</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to="/workspace" className="flex gap-2  text-slate-700 hover:bg-slate-100 transition-colors rounded-lg py-1 px-2" onClick={()=> setIsSidebarOpen(false)}>
+                        <NavLink to="/workspace" className={({isActive})=>`flex gap-2 rounded-lg py-1 px-2 transition-colors ${isActive?"bg-blue-100 text-blue-600":"text-slate-600 hover:bg-slate-100"}`} onClick={()=> setIsSidebarOpen(false)}>
                             <BriefcaseBusiness size={22} className="font-bold" /> <span className="text-base "> Workspace</span>
-                        </Link>
+                        </NavLink>
                     </li>
                                         <li>
-                        <Link to="/invitation" className="flex justify-between  text-slate-700 hover:bg-slate-100 transition-colors rounded-lg py-1 px-2" onClick={()=> setIsSidebarOpen(false)}>
+                        <NavLink to="/invitation" className={({isActive})=>`flex justify-between transition-colors rounded-lg py-1 px-2 ${isActive?"bg-blue-100 text-blue-600":"text-slate-600 hover:bg-slate-100"}`} onClick={()=> setIsSidebarOpen(false)}>
                         <div className="flex gap-2">
                              < Mail size={22} className="font-bold" /> <span className="text-base "> Invitations</span>
                         </div>
                            
                             <div className="bg-blue-600 text-white rounded-full h-5 w-5 flex justify-center items-center">{"2"}</div>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to="/project" className="flex gap-2  text-slate-700 hover:bg-slate-100 transition-colors rounded-lg py-1 px-2" onClick={()=> setIsSidebarOpen(false)}>
+                        <NavLink to="/project" className={({isActive})=>`flex gap-2 rounded-lg py-1 px-2 transition-colors ${isActive?"bg-blue-100 text-blue-600":"text-slate-600 hover:bg-slate-100"}`} onClick={()=> setIsSidebarOpen(false)}>
                             <FolderKanban size={22} className="font-bold" /> <span className="text-base">Projects</span>
-                        </Link>
+                        </NavLink>
                     </li>
                     <li>
-                        <Link to="/task" className="flex gap-2  text-slate-700 hover:bg-slate-100 transition-colors rounded-lg py-1 px-2" onClick={()=> setIsSidebarOpen(false)}>
+                        <NavLink to="/task" className={({isActive})=>`flex gap-2 rounded-lg py-1 px-2 transition-colors ${isActive?"bg-blue-100 text-blue-600":"text-slate-600 hover:bg-slate-100"}`} onClick={()=> setIsSidebarOpen(false)}>
                             <ListTodo size={22} className=" font-bold" /> <span className="text-base ">Tasks</span>
-                        </Link>
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
@@ -49,3 +51,9 @@ function Sidebar({ isSidebarOpen, setIsSidebarOpen }) {
     )
 }
 export default Sidebar
+
+//flex gap-2  text-slate-700 hover:bg-slate-100 transition-colors rounded-lg py-1 px-2
+
+//flex gap-2  bg-blue-100 rounded-lg py-1 px-2 text-blue-700
+
+//flex justify-between  text-slate-700 hover:bg-slate-100 transition-colors rounded-lg py-1 px-2
