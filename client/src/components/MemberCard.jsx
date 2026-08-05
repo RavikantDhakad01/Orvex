1b8182f0c9f9459d3d5f06199c553768309c1711
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-08-05 09:54:24 +0530

    improve spacing

--- client/src/components/MemberCard.jsx
+++ client/src/components/MemberCard.jsx
@@ -3,9 +3,11 @@ import { EllipsisVertical } from "lucide-react"
 import OwnerBadge from "./OwnerBadge.jsx"
 function MemberCard({ member, isOwner }) {
     return (
-        <div className="flex gap-6 items-center rounded-lg">
-            <Avatar className="rounded-full" name={member.username} color={member.avatarColor} />
+        <div className="flex gap-10 items-center rounded-lg">
+            <div className="flex items-center gap-4">
+                  <Avatar className="rounded-full" name={member.username} color={member.avatarColor} />
             <span className="font-bold text-lg">{member.username}</span>
+            </div>
            {isOwner && <OwnerBadge />}
         </div>
     )
