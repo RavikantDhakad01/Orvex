8191b65dc55bbe6e6f14c58ebce76dfc6b4fa6bf
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-08-10 17:24:50 +0530

    Add permission model for project

--- server/src/middlewares/Permission.middleware.js
+++ server/src/middlewares/Permission.middleware.js
@@ -1,5 +1,6 @@
 import ApiError from "../utils/ApiError.js"
 import WorkspaceMember from "../models/workspaceMember.model.js"
+import Project from "../models/project.model.js"
 
 const authorizeWorkspace = (roles = []) => {
     return async (req, res, next) => {
@@ -33,4 +34,43 @@ const authorizeWorkspace = (roles = []) => {
         }
     }
 }
-export { authorizeWorkspace }
+
+const authorizeProject = (roles = []) => {
+    return async (req, res, next) => {
+        try {
+            const { projectId } = req.params
+            if (!projectId) {
+                throw new ApiError(400, "Project id is required")
+            }
+
+            const project = await Project.findById(projectId)
+
+            if (!project) {
+                throw new ApiError(404, "Project does not exist")
+            }
+            const workspaceId = project.workspace
+            const workspaceMember = await WorkspaceMember.findOne({
+                workspace: workspaceId,
+                user: req.user._id
+            })
+
+            if (!workspaceMember) {
+                throw new ApiError(403, "User is not the member of workspace")
+            }
+
+            const userRole = workspaceMember.role
+
+            if (!roles.includes(userRole)) {
+                throw new ApiError(403, "You don't have permit to perform this action")
+            }
+
+            req.workspaceMember = workspaceMember
+            req.project = project
+            next()
+
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+export { authorizeWorkspace, authorizeProject }
