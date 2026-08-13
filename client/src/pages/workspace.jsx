4c7bbcd084ca784a4afd0511a957f87eb55ca7d6
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-08-13 18:38:51 +0530

    formate document

--- client/src/pages/workspace.jsx
+++ client/src/pages/workspace.jsx
@@ -1,77 +1,106 @@
-import Button from "../components/Button.jsx"
-import WorkspaceCard from "../components/WorkspaceCard.jsx"
-import { useState, useEffect } from "react"
-import Model from "../components/Model.jsx"
-import CreateModel from "../components/CreateModel.jsx"
-import EmptyStats from "../components/EmptyStats.jsx"
-import { BriefcaseBusiness } from "lucide-react"
-import { getUserWorkspaces, createWorkspace } from "../services/workspace.services.js"
-import toast from "react-hot-toast";
-import Loader from "../components/Loader.jsx"
-import { useNavigate } from "react-router-dom"
-useNavigate
+import Button from '../components/Button.jsx';
+import WorkspaceCard from '../components/WorkspaceCard.jsx';
+import { useState, useEffect } from 'react';
+import Model from '../components/Model.jsx';
+import CreateModel from '../components/CreateModel.jsx';
+import EmptyStats from '../components/EmptyStats.jsx';
+import { BriefcaseBusiness } from 'lucide-react';
+import {
+  getUserWorkspaces,
+  createWorkspace,
+} from '../services/workspace.services.js';
+import toast from 'react-hot-toast';
+import Loader from '../components/Loader.jsx';
+import { useNavigate } from 'react-router-dom';
+useNavigate;
 
 function Workspace() {
-    const [isModelOpen, setModelOpen] = useState(false)
-    const [loading, setLoading] = useState(true)
-    const [workspaces, setWorkspaces] = useState([])
-    const navigate = useNavigate()
+  const [isModelOpen, setModelOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [workspaces, setWorkspaces] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchWorkspaces = async () => {
+      try {
+        const workspaces = await getUserWorkspaces();
+        setWorkspaces(workspaces.data);
+      } catch (error) {
+        if (error.response) {
+          toast.error(
+            error.response.data.message ||
+              'Something went wrong. Please refresh'
+          );
+        } else if (error.request) {
+          toast.error('Please check your internet connection');
+        } else {
+          toast.error('Something went wrong. Please refresh');
+        }
 
-        const fetchWorkspaces = async () => {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            try {
-                const workspaces = await getUserWorkspaces();
-                setWorkspaces(workspaces.data);
-            } catch (error) {
-                if (error.response) {
-                    toast.error(error.response.data.message || "Something went wrong. Please refresh");
-                } else if (error.request) {
-                    toast.error("Please check your internet connection");
-                } else {
-                    toast.error("Something went wrong. Please refresh");
-                }
+    fetchWorkspaces();
+  }, []);
 
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchWorkspaces();
-
-    }, []);
-
-    if (loading) {
-        return <Loader />;
-    }
-    return (
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <>
+      {workspaces.length > 0 ? (
         <>
-            {
-                workspaces.length > 0 ? (
-                    <>
-                        <div className="flex justify-between">
-                            <h1 className="text-2xl font-bold">Workspaces</h1>
-                            <Button text="+ Create" className="py-1 cursor-pointer" onClick={() => setModelOpen(true)} />
-                        </div>
+          <div className="flex justify-between">
+            <h1 className="text-2xl font-bold">Workspaces</h1>
+            <Button
+              text="+ Create"
+              className="py-1 cursor-pointer"
+              onClick={() => setModelOpen(true)}
+            />
+          </div>
 
-                        <div className="flex flex-col gap-4 mt-6">
-
-                            {workspaces.map((w) => <WorkspaceCard workspaceDetails={w} key={w.workspace._id} onClick={() => {
-navigate(`/workspace/${w.workspace._id}`)
-                            }} />)}
-
-                        </div>
-                    </>) : (<>
-                        <EmptyStats icon={<BriefcaseBusiness size={54}
-                            strokeWidth={1.25} className="text-blue-500" />} heading="No workspaces yet" note="Get started by creating your first workspace">
-                            <Button text="+ Create Workspace" className="w-full py-4 cursor-pointer" onClick={() => setModelOpen(true)} />
-                        </EmptyStats>
-                    </>)
-            }
-            {isModelOpen && <Model><CreateModel setModelOpen={setModelOpen} /></Model>}
+          <div className="flex flex-col gap-4 mt-6">
+            {workspaces.map((w) => (
+              <WorkspaceCard
+                workspaceDetails={w}
+                key={w.workspace._id}
+                onClick={() => {
+                  navigate(`/workspace/${w.workspace._id}`);
+                }}
+              />
+            ))}
+          </div>
         </>
-    )
+      ) : (
+        <>
+          <EmptyStats
+            icon={
+              <BriefcaseBusiness
+                size={54}
+                strokeWidth={1.25}
+                className="text-blue-500"
+              />
+            }
+            heading="No workspaces yet"
+            note="Get started by creating your first workspace"
+          >
+            <Button
+              text="+ Create Workspace"
+              className="w-full py-4 cursor-pointer"
+              onClick={() => setModelOpen(true)}
+            />
+          </EmptyStats>
+        </>
+      )}
+      {isModelOpen && (
+        <Model>
+          <CreateModel setModelOpen={setModelOpen} />
+        </Model>
+      )}
+    </>
+  );
 }
-export default Workspace
+export default Workspace;
