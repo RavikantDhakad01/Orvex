a440f8d50be27e89f2b878f2d311ed62dd9446db
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-08-15 17:42:30 +0530

    fetch extra info

--- server/src/middlewares/Permission.middleware.js
+++ server/src/middlewares/Permission.middleware.js
@@ -25,8 +25,6 @@ const authorizeWorkspace = (roles = []) => {
             if (!roles.includes(userRole)) {
                 throw new ApiError(403, "You don't have permit to perform this action")
             }
-
-            req.workspaceMember = workspaceMember
             next()
 
         } catch (error) {
@@ -43,7 +41,7 @@ const authorizeProject = (roles = []) => {
                 throw new ApiError(400, "Project id is required")
             }
 
-            const project = await Project.findById(projectId)
+            const project = await Project.findById(projectId).populate("workspace","owner")
 
             if (!project) {
                 throw new ApiError(404, "Project does not exist")
@@ -64,7 +62,6 @@ const authorizeProject = (roles = []) => {
                 throw new ApiError(403, "You don't have permit to perform this action")
             }
 
-            req.workspaceMember = workspaceMember
             req.project = project
             next()
 
