dfbe814e91ffedd5b31de3c33249427da902e898
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-08-15 17:39:26 +0530

    Add on click

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,10 +1,12 @@
 import Avatar from "./Avatar.jsx"
 import { EllipsisVertical } from "lucide-react"
 import {getTimeAgo} from "../utils/getTimeAgo.js"
+import { useNavigate } from "react-router-dom"
 
 function ProjectCard({project}) {
+  const navigate=useNavigate()
   return (
-    <div className="flex justify-between items-center shadow-md py-6 px-4 rounded-lg">
+    <div className="flex justify-between items-center shadow-md py-6 px-4 rounded-lg cursor-pointer" onClick={()=>navigate(`/project/${project._id}`)}>
       <div className="flex gap-6 items-center">
         <Avatar className="rounded-full" name={project.name} color={project.avatarColor}/>
         <div className="flex flex-col">
