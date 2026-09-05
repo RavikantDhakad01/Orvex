b975848eb248780290de5829c7faa685a653f852
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-09-05 17:41:19 +0530

    rename function

--- client/src/components/Model.jsx
+++ client/src/components/Model.jsx
@@ -1,4 +1,4 @@
-function Modal({children}) {
+function Model({children}) {
     return (
         <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
             <div className="bg-white shadow-lg rounded-xl p-6 w-[90%] max-w-md">
@@ -8,4 +8,4 @@ function Modal({children}) {
     )
 }
 
-export default Modal
+export default Model
